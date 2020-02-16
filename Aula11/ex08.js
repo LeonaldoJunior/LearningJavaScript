90c8d9354a44b226f784a5eead94a5bd471a6c73
Leonaldo <leonaldo.barbosa@gmail.com>
2020-02-16 14:51:15 -0300

    Aula 11 Conditions

---
+++ Aula11/ex08.js
@@ -0,0 +1,10 @@
+console.log('O console funcionou corretamente')
+
+var vel = 60.5
+console.log(`A velocidade do seu carro eh ${vel}`);
+
+if(vel > 60){
+    console.log(`Voce ultrapassou a velocidade permisita. MULTADO!`);
+}
+
+console.log(`Use cinto de segurança!!`);
